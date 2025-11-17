72c10f21fb7e836e53dff0c7162121c9a7723c84
Yash <yash27bhatewara@gmail.com>
2025-11-17 16:21:29 +0530

    basic code done

---
+++ mlartifacts/523478073335633636/21a435b87a0344dab70acdc327c3dab9/artifacts/file1.py
@@ -0,0 +1,67 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from mlflow.models.signature import infer_signature
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# Load Wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+X_train = pd.DataFrame(X_train, columns=wine.feature_names)
+X_test  = pd.DataFrame(X_test,  columns=wine.feature_names)
+
+# Define the params for RF model
+max_depth = 10
+n_estimators = 8
+
+# Mention your experiment below
+mlflow.set_experiment('Exp1')
+
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    rf.fit(X_train, y_train)
+
+    y_pred = rf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric('accuracy', accuracy)
+    mlflow.log_param('max_depth', max_depth)
+    mlflow.log_param('n_estimators', n_estimators)
+
+    # Creating a confusion matrix plot
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+
+    # save plot
+    plt.savefig("Confusion-matrix.png")
+
+    # log artifacts using mlflow
+    mlflow.log_artifact("Confusion-matrix.png")
+    mlflow.log_artifact(__file__)
+
+    # tags
+    mlflow.set_tags({"Author": 'Yash', "Project": "Wine Classification"})
+
+    signature = infer_signature(X_train, rf.predict(X_train.to_numpy()))
+    input_example = X_train.head(5)
+    
+    # Log the model
+    mlflow.sklearn.log_model(rf, name="Random-Forest-Model", signature=signature, input_example=input_example)
+
+    print(accuracy)
